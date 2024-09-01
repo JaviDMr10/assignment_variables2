cc4c9a130e40ca606d289b2f7b460fa9208b67e8
JaviDMr10 <javidmr10@gmail.com>
2024-09-01 01:44:25 -0400

    Initial commit

---
+++ variableAssignment.py
@@ -0,0 +1,44 @@
+# assignment2: Variables
+# 5 multiplied by 6
+
+math1 = 5 * 6
+print("5 multiplied by 6")
+print("The answer to your query:", math1)
+print("")
+
+# 8 divided by 3
+
+math2 = 8 / 3
+print("8 divided by 3")
+print("The answer to your query:", math2)
+print("")
+
+# 8 divided by 3 without the remainder
+
+math3 = 8 // 3
+print("8 divided by 3 without the remainder")
+print("The answer to your query:", math3)
+print("")
+
+# 8 modulo 3 (remainder only)
+
+math4 = 8 % 3
+print("8 modulo 3")
+print("The answer to your query:", math4)
+print("")
+
+# 4200000 add 23090
+
+math5 = 4200000 + 23090
+print("4200000 add 23090")
+print("The answer to your query:", math5)
+print("")
+
+# 4200000 subtract 7633
+
+math6 = 4200000 - 7633
+print("4200000 subtract 7633")
+print("The answer to your query:", math6)
+print("")
+
+print("Javier D. Moreno")
